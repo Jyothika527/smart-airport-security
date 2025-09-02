cb3933e827f6939a5bb7da252442abb39dbdd8ee
Jyothika527 <jyothikat905@gmail.com>
2025-09-02 20:00:40 +0530

    Update airport_security.v

--- airport_security/airport_security.v
+++ airport_security/airport_security.v
@@ -11,12 +11,17 @@ module airport_security (
   output [7:0] security_token     // Random ID
 );
 
+  wire priority_out;
+  wire [2:0] priority_in = 3'b101; // Example: VIP=1, Business=0, Regular=1
+  wire [7:0] id;
+
   // Priority Checker
   priority_checker pc (
     .sel(passenger_type),
-    .sensor_input(sensor_pulse),
-    .priority_out(priority)
+    .priority_in(priority_in),
+    .priority_out(priority_out)
   );
+  assign priority = {1'b0, priority_out}; // Pad to 2 bits
 
   // Object Counter
   object_counter oc (
@@ -29,21 +34,22 @@ module airport_security (
   // Parity Checker
   parity_checker pchk (
     .data(baggage_data),
-    .parity_bit(parity)
+    .parity(parity)
   );
 
   // Traffic Light Controller
   traffic_light tlc (
     .clk(clk),
     .reset(reset),
-    .light_out(light)
+    .light(light)
   );
 
   // Random ID Generator
   id_generator idgen (
     .clk(clk),
     .reset(reset),
-    .id_out(security_token)
+    .id(id)
   );
+  assign security_token = id;
 
 endmodule
