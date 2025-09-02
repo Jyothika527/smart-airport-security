c0978f7b264a13c5dcb77386f50e7d87de256e91
Jyothika527 <jyothikat905@gmail.com>
2025-09-02 12:17:07 +0000

    Add traffic light FSM module and testbench

---
+++ traffic_light/traffic_light_tb.v
@@ -0,0 +1,18 @@
+module traffic_light_tb;
+  reg clk, reset;
+  wire [1:0] light;
+
+  traffic_light uut (.clk(clk), .reset(reset), .light(light));
+
+  initial begin
+    $dumpfile("traffic.vcd"); $dumpvars(0, tb_traffic_light);
+    clk = 0; reset = 1; #5;
+    reset = 0;
+
+    repeat (10) begin
+      #5 clk = ~clk;
+    end
+
+    $finish;
+  end
+endmodule
