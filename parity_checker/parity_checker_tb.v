714f20068764fc3df1e0773ecf6272c93295d7b6
Jyothika527 <jyothikat905@gmail.com>
2025-09-02 18:38:05 +0530

    Update parity_checker_tb.v

--- parity_checker/parity_checker_tb.v
+++ parity_checker/parity_checker_tb.v
@@ -5,7 +5,7 @@ module tb_parity_checker;
   parity_checker uut (.data(data), .parity(parity));
 
   initial begin
-    $dumpfile("parity.vcd"); $dumpvars(0, tb_parity_checker);
+    $dumpfile("parity.vcd"); $dumpvars(0, parity_checker_tb);
     data = 8'b10101010; #10;
     data = 8'b11110000; #10;
     data = 8'b00000000; #10;
